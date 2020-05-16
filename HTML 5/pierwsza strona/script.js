6b56030e1ec171b26d3a1bc1814a574fd38cb932
Kacper Szewczyk <mistrzszefow@gmail.com>
2020-05-16 13:12:13 +0200

    Skrypt JS i komentarze

---
+++ HTML 5/pierwsza strona/script.js
@@ -0,0 +1,12 @@
+
+function counter() {
+    console.log("Witaj Świecie z pliku");
+    for (let i=0; i<5; i++) {
+        console.log(i);
+        // debugger;
+        /* wiele linii komentarza
+        */
+    }
+}
+
+counter();
