1ae9fccbaa45865074cf019b856bb4937821cf0c
Kacper Szewczyk <mistrzszefow@gmail.com>
2020-05-23 13:28:22 +0200

    ustawianie stanu aplikacji

--- pierwsza-aplikacja/src/components/list-alphabet/ListAlphabet.js
+++ pierwsza-aplikacja/src/components/list-alphabet/ListAlphabet.js
@@ -7,18 +7,26 @@ class ListAlphabet extends Component {
     constructor(props) {
         super(props);
         this.state = {
+            justClicked: null,
             letters: Array.from({length: 26}, (_,i) => {
                 return String.fromCharCode(i + A);
             })
         }
     }
 
+    handleClick(letter) {
+        this.setState({ justClicked: letter});
+    }
+
     render() {
         return (
-            <div>
+            <div className="separated">
+                Just clicked: {this.state.justClicked}
                 <ul>
                     {this.state.letters.map(letter =>
-                        <li key={letter}>{letter}</li>
+                        <li key={letter} onClick={() => this.handleClick(letter)}>
+                            {letter}
+                        </li>
                     )}
                 </ul>
             </div>
