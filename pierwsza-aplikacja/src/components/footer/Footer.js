de60ecaed1036e2e2e93cb480b9f3c0f73ac5208
Kacper Szewczyk <mistrzszefow@gmail.com>
2020-05-23 11:09:19 +0200

    Pierwsza apka react

---
+++ pierwsza-aplikacja/src/components/footer/Footer.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './Footer.css';
+
+function Footer() {
+    return (
+        <footer className="App-footer">
+            <div>
+                Test
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
